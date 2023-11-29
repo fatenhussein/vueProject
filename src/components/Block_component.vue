31b8f47ff59fed66b5cb2f926186f9c333c6c2cf
faten282 <112074192+fatenhussein@users.noreply.github.com>
2023-11-29 19:51:29 +0300

    faten

---
+++ src/components/Block_component.vue
@@ -0,0 +1,32 @@
+<template >
+
+
+
+
+<div class="block">
+    clcke me 
+</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+    
+
+    .block{
+
+        width: 400px;
+
+        border-radius: 20px;
+
+        background-color: aquamarine;
+        color: white;
+
+        text-align: center;
+        padding: 100px 0px ;
+
+        margin: 40px auto;
+    }
+</style>
